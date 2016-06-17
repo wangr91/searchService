a5ce1fca75abe6e5f32dc415f692b2241b657cfa
wangr91@chinaunicom.cn <wangr91@chinaunicom.cn>
2016-06-17 13:01:15 +0800

    99

---
+++ SearchService/src/org/shirdrn/platform/dubbo/service/rpc/server/TestSolrSearchServer.java
@@ -0,0 +1,15 @@
+package org.shirdrn.platform.dubbo.service.rpc.server;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class TestSolrSearchServer {
+	@Test
+	public void  start() throws IOException {
+		//String config = SolrSearchServer.class.getPackage().getName().replace('.', '/') + "/search-provider.xml";
+		String config =  "search-provider.xml";
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(config);
+        context.start();
+        System.in.read();
+	}
+}
